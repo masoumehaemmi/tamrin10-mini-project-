64e0e956b354219b8dcb3e776eea1eab7fa62429
masoumehaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-04 12:50:46 +0430

    Add files via upload

---
+++ media.py
@@ -0,0 +1,42 @@
+import ast
+import pytube
+from pytube import YouTube
+
+class Media:
+    def __init__(self,id,n,di,im,u,du,c):
+        self.id=id
+        self.name=n
+        self.director=di
+        self.imd_score=im
+        self.url=u
+        self.duration=du
+        self.casts=c
+
+    def showInfo(self):
+        self.list=[]
+        self.mydict={}
+        myfile=open("databasefilm.txt","r")
+        rows=myfile.read().split("\n")
+        for i in range(len(rows)):
+            medi=rows[i].split(",")
+            self.mydict["id"]=int(medi[0])
+            self.mydict["name"]=medi[1]
+            self.mydict["director"]=medi[2]
+            self.mydict["IMDB"]=float(medi[3])
+            self.mydict["url"]=medi[4]
+            self.mydict["duration"]=int(medi[5])
+            self.mydict["casts"]=medi[6]
+            print(self.mydict)
+            self.list.append(self.mydict)
+        #return self.list  
+
+    def download(self):
+        link='http://www.youtube.com/watch?v=lVFNRrL79w0'
+        first_stream=pytube.YouTube(link).streams.first()
+        first_stream.download(output_path='./',filename="test.mp4")
+        #print(first_stream)
+    
+
+
+
+   
