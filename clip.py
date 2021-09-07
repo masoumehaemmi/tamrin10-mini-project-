1ed0fe3a65590625911721ae197d585d67eedf38
masoumehaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-07 23:53:00 +0430

    Add files via upload

--- clip.py
+++ clip.py
@@ -1,3 +1,6 @@
 import media
+
 class Clip(media.Media):
-    pass
+    def __init__(self,media):
+        media.__init__(self,media[0],media[1],media[2],media[3],media[4],media[5],media[6])
+        self.episod=media[7]
