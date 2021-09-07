2bc7831aac2b3e0a01c1cb11384ec2c3337bfd58
masoumehaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-07 23:59:18 +0430

    Add files via upload

--- series.py
+++ series.py
@@ -1,3 +1,5 @@
 import media
 class Series(media.Media):
-    pass
+    def __init__(self,media):
+        media.__init__(self,media[0],media[1],media[2],media[3],media[4],media[5],media[6])
+        self.episode=media[7]        
