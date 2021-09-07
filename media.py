7930c0bb241fa26acf538feab2cdaec3e6248786
masoumehaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-07 23:58:41 +0430

    Add files via upload

--- media.py
+++ media.py
@@ -3,32 +3,34 @@ import pytube
 from pytube import YouTube
 
 class Media:
-    def __init__(self,id,n,di,im,u,du,c):
+    def __init__(self,id,c,n,di,im,u,du,ca):
         self.id=id
+        self.categori=c
         self.name=n
         self.director=di
-        self.imd_score=im
+        self.IMDB_score=im
         self.url=u
         self.duration=du
-        self.casts=c
+        self.casts=ca
 
-    def showInfo(self):
-        self.list=[]
-        self.mydict={}
-        myfile=open("databasefilm.txt","r")
-        rows=myfile.read().split("\n")
-        for i in range(len(rows)):
-            medi=rows[i].split(",")
-            self.mydict["id"]=int(medi[0])
-            self.mydict["name"]=medi[1]
-            self.mydict["director"]=medi[2]
-            self.mydict["IMDB"]=float(medi[3])
-            self.mydict["url"]=medi[4]
-            self.mydict["duration"]=int(medi[5])
-            self.mydict["casts"]=medi[6]
-            print(self.mydict)
-            self.list.append(self.mydict)
-        #return self.list  
+    def show(self):
+        # self.list=[]
+        # self.mydict={}
+        # myfile=open("databasefilm.txt","r")
+        # rows=myfile.read().split("\n")
+        # for i in range(len(rows)):
+        #     medi=rows[i].split(",")
+        #     self.mydict["id"]=int(medi[0])
+        #     self.mydict["name"]=medi[1]
+        #     self.mydict["director"]=medi[2]
+        #     self.mydict["IMDB"]=float(medi[3])
+        #     self.mydict["url"]=medi[4]
+        #     self.mydict["duration"]=int(medi[5])
+        #     self.mydict["casts"]=medi[6]
+        #     print(self.mydict)
+        #     self.list.append(self.mydict)
+        # #return self.list  
+        print(f"id : {self.id} *** categori : {self.categori} *** name : {self.name} *** director : {self.director} *** IMDB : {self.IMDB_score} ")
 
     def download(self):
         link='http://www.youtube.com/watch?v=lVFNRrL79w0'
