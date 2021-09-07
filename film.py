d6bbe61e57e3635e0a1d37a33840f77610e27a52
masoumehaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-07 23:57:19 +0430

    Add files via upload

--- film.py
+++ film.py
@@ -1,26 +1,30 @@
-import media
+from media import Media
 
-class Film(media.Media):
+class Film(Media):
     
-    def __init__(self):
-        self.list=[]
-        mydict={}
-        myfile=open("databasefilm.txt","r")
-        rows=myfile.read().split("\n")
-        for i in range(len(rows)):
-            media=rows[i].split(",")
-            
-            mydict["id"]=int(media[0])
-            mydict["name"]=media[1]
-            mydict["director"]=media[2]
-            mydict["IMDB"]=float(media[3])
-            mydict["url"]=media[4]
-            mydict["duration"]=int(media[5])
-            mydict["casts"]=media[6]
-            self.list.append(self.mydict)
-        return self.list  
+    def __init__(self,media):
+        
+        media.__init__(self,media[0],media[1],media[2],media[3],media[4],media[5],media[6])
 
-    def show(self):
-        print()
-f=Film()
-print(f)
+    
+#         self.list=[]
+#         mydict={}
+#         myfile=open("databasefilm.txt","r")
+#         rows=myfile.read().split("\n")
+#         for i in range(len(rows)):
+#             media=rows[i].split(",")
+            
+#             mydict["id"]=int(media[0])
+#             mydict["name"]=media[1]
+#             mydict["director"]=media[2]
+#             mydict["IMDB"]=float(media[3])
+#             mydict["url"]=media[4]
+#             mydict["duration"]=int(media[5])
+#             mydict["casts"]=media[6]
+#             self.list.append(self.mydict)
+#         return self.list  
+
+#     def show(self):
+#         print()
+# # f=Film()
+# # print(f)
