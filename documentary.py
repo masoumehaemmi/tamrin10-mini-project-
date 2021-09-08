e75d02b7bccf813cbe485d7480b0cef9187be0e4
masoumehaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-08 11:38:29 +0430

    Update documentary.py

--- documentary.py
+++ documentary.py
@@ -1,5 +1,6 @@
 from media import Media
 class Documentary(Media):
     def __init__(self,media):
-        media.__init__(self,media[0],media[1],media[2],media[3],media[4],media[5],media[6])
+
+        Media.__init__(self,media[0],media[1],media[2],media[3],media[4],media[5],media[6])
         self.episod=media[7]
