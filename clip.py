6c64e5bda292c84af20d646563109d52639307b9
masoumehaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-08 11:36:10 +0430

    Update clip.py

--- clip.py
+++ clip.py
@@ -2,5 +2,5 @@ from media import Media
 
 class Clip(Media):
     def __init__(self,media):
-        media.__init__(self,media[0],media[1],media[2],media[3],media[4],media[5],media[6])
+        Media.__init__(self,media[0],media[1],media[2],media[3],media[4],media[5],media[6])
         self.episod=media[7]
